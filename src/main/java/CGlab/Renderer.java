e094e24b210e7fa87a07380f7511d3101b74105a
cyboranf <cyboranf@gmail.com>
2023-03-13 10:44:20 +0100

    ex 2.4

--- src/main/java/CGlab/Renderer.java
+++ src/main/java/CGlab/Renderer.java
@@ -1,7 +1,7 @@
 package CGlab;
 
 
-import java.awt.Graphics2D;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -23,7 +23,7 @@ public class Renderer {
         this.filename = filename;
     }
 
-    public void drawPoint(int x, int y) {
+    public void drawPoint(int x, int y, Color color) {
         int white = 255 | (255 << 8) | (255 << 16) | (255 << 24);
         render.setRGB(x, y, white);
     }
@@ -58,14 +58,14 @@ public class Renderer {
         // TODO: zaimplementuj
         int white = 255 | (255 << 8) | (255 << 16) | (255 << 24);
 
-        int dx = x1-x0;
-        int dy = y1-y0;
-        float derr = Math.abs(dy/(float)(dx));
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+        float derr = Math.abs(dy / (float) (dx));
         float err = 0;
 
         int y = y0;
 
-        for (int x=x0; x<=x1; x++) {
+        for (int x = x0; x <= x1; x++) {
             render.setRGB(x, y, white);
             err += derr;
             if (err > 0.5) {
@@ -78,6 +78,21 @@ public class Renderer {
 
     public void drawLineBresenhamInt(int x0, int y0, int x1, int y1) {
         // TODO: zaimplementuj
+        if (Math.abs(y1 - y0) < Math.abs(x1 - x0)) {
+            if (x0 > x1) {
+                plotLineLow(x1, y1, x0, y0);
+            } else {
+                plotLineLow(x0, y0, x1, y1);
+            }
+        } else {
+            if (y0 > y1) {
+                plotLineHigh(x1, y1, x0, y0);
+            } else {
+                plotLineHigh(x0, y0, x1, y1);
+            }
+        }
+
+        //plotLineLow and plotLineHigh are in bottom of this class
     }
 
     public void save() throws IOException {
@@ -103,5 +118,50 @@ public class Renderer {
         g.drawImage(img, 0, 0, w, h, 0, h, w, 0, null);
         g.dispose();
         return flippedImage;
+    }
+
+
+    private void plotLineLow(int x0, int y0, int x1, int y1) {
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+        int yi = 1;
+        if (dy < 0) {
+            yi = -1;
+            dy = -dy;
+        }
+        int D = (2 * dy) - dx;
+        int y = y0;
+
+        for (int x = x0; x < x1; x++) {
+            drawPoint(x, y, new Color(255, 255, 0));
+            if (D > 0) {
+                y = y + yi;
+                D = D + (2 * (dy - dx));
+            } else {
+                D = D + 2 * dy;
+            }
+        }
+    }
+
+    private void plotLineHigh(int x0, int y0, int x1, int y1) {
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+        int xi = 1;
+        if (dx < 0) {
+            xi = -1;
+            dx = -dx;
+        }
+        int D = (2 * dx) - dy;
+        int x = x0;
+
+        for (int y = y0; y < y1; y++) {
+            drawPoint(x, y, new Color(0, 0, 0));
+            if (D > 0) {
+                D = D + (2 * (dx - dy));
+            }else{
+                D = D + 2*dx;
+            }
+        }
+
     }
 }
